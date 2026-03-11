043504ce1890e645b6d77225f543aedf8e3f3f2e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-12 00:21:27 +0100

    Update splashscreen image

--- app/home.tsx
+++ app/home.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, Image, Text,     } from 'react-native'
 const home = () => {
   return (
-    <View>
+    <View style={styles.view}>
 
+   <Image source={require('@/assets/images/image 1.png')}   />
+    <Text style={{fontSize: 30, color: "#ffffff",  }}> Track your expenses with SmartSpend</Text>
     </View>
   )
-}
+};
+const styles = StyleSheet.create({
+view: {
+height: "100%",
+backgroundColor: "#04091E",
+ display: "flex",
+ alignItems: "center",
+ textAlign: "center",
+
+
+    }
+})
 
 export default home
