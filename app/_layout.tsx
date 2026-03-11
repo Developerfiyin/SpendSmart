b22b2dc4860f9ad62af880dacaaecfefd0e8fa0c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-11 12:07:57 +0100

    Update app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,9 @@
 import { Stack } from "expo-router";
 
-
-
 export default function TabsLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 };
