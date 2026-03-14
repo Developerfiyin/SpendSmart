13161865aefeb603c94b87af7641f57a9459d285
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-14 21:35:30 +0100

    Update home tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image, Pressable, Text, View } from "react-native";
-
+import Spa
 export default function Index() {
   return (
     <View
@@ -19,9 +19,10 @@ export default function Index() {
           gap: 8,
         }}
       >
-        <Text style={{fontSize: 30, fontWeight: "black", color: "#fff", textAlign: "left", marginHorizontal: 10 }}>
+        
+        <Span style={{fontSize: 30, fontWeight: "black", color: "#fff", textAlign: "left", marginHorizontal: 10 }}>
           AMY JONES
-        </Text>
+        </Span>
         {/* THE ICONS*/}
         <Pressable style={{ flexDirection: "row", gap: 9, right: 12 }}>
           <Image source={require("@/assets/images/searchIcon.png")} />
